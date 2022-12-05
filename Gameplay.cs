4f3477870535a3d54eea43e9bf029fbcede5a9c1
rajpal229 <rajpalrawat229@gmail.com>
2022-12-06 01:19:50 +0530

    Added: UC7_TwoPlayerGame

--- Gameplay.cs
+++ Gameplay.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +10,10 @@ namespace Snake_And_Ladder_Problem
     internal class Gameplay
     {
         Random random = new Random();
-        int WINNING_POSITION = 100;
-        int playerPosition = 0;
+        int WINNING_POSITION = 100, turn = 1;
+        int player_1_Position = 0, player_2_Position = 0;
+
+        
         public void Position()
         {
             int playerPosition = 0;
@@ -27,46 +30,110 @@ namespace Snake_And_Ladder_Problem
         }
         public void Check()
         {
-            int chance = 0;
-            int die = DiceRoll();
-            while (playerPosition < WINNING_POSITION)
+            int chance1 = 0, chance2 = 0;
+            while (player_1_Position < WINNING_POSITION && player_2_Position < WINNING_POSITION)
             {
-                int ROLL = random.Next(0, 3);
-                //Console.WriteLine(ROLL);
-                switch (ROLL)
+                void PlayerSelection()
                 {
-                    case 0:
-                        DiceRoll();
-                        Console.WriteLine("No Play: Stay on your position");
-                        playerPosition += 0;
-                        break;
-                    case 1:
-                        playerPosition += DiceRoll();
-                        if(playerPosition > WINNING_POSITION)
-                        {
-                            playerPosition -= DiceRoll();
-                            Console.WriteLine("No Play: Stay on your position");
+                    switch (turn)
+                    {
+                        case 1:
+                            Console.WriteLine("Chance of Player 1");
                             break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Ladder: Move Forward");
-                        }
-                        break;
-                    case 2:
-                        playerPosition -= DiceRoll();
-                        Console.WriteLine("Snake: Go Back");
-                        if(playerPosition < 0)
-                        {
-                            playerPosition = 0;
-                        }
-                        break;
+                        default:
+                            Console.WriteLine("Chance of Player 2");
+                            break;
+                    }
                 }
-                chance += 1;
-                Console.WriteLine("You're New Position - "+playerPosition);
+                if (turn == 1)
+                {
+                    PlayerSelection();
+                    Console.WriteLine("Player1 is at: "+player_1_Position);
+                    int ROLL = random.Next(0, 3);
+                    //Console.WriteLine(ROLL);
+                    switch (ROLL)
+                    {
+                        case 0:
+                            DiceRoll();
+                            Console.WriteLine("No Play: Stay on your position");
+                            player_1_Position += 0;
+                            break;
+                        case 1:
+                            player_1_Position += DiceRoll();
+                            if (player_1_Position > WINNING_POSITION)
+                            {
+                                player_1_Position -= DiceRoll();
+                                Console.WriteLine("No Play: Stay on your position");
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ladder: Move Forward");
+                            }
+                            break;
+                        case 2:
+                            player_1_Position -= DiceRoll();
+                            Console.WriteLine("Snake: Go Back");
+                            if (player_1_Position < 0)
+                            {
+                                player_1_Position = 0;
+                            }
+                            break;
+                    }
+                    chance1 += 1;
+                    Console.WriteLine("You're New Position - " + player_1_Position);
+                    turn += 1;
+                }
+                else
+                {
+                    PlayerSelection();
+                    Console.WriteLine("Player2 is at: " + player_2_Position);
+                    int ROLL = random.Next(0, 3);
+                    //Console.WriteLine(ROLL);
+                    switch (ROLL)
+                    {
+                        case 0:
+                            DiceRoll();
+                            Console.WriteLine("No Play: Stay on your position");
+                            player_2_Position += 0;
+                            break;
+                        case 1:
+                            player_2_Position += DiceRoll();
+                            if (player_2_Position > WINNING_POSITION)
+                            {
+                                player_2_Position -= DiceRoll();
+                                Console.WriteLine("No Play: Stay on your position");
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ladder: Move Forward");
+                            }
+                            break;
+                        case 2:
+                            player_2_Position -= DiceRoll();
+                            Console.WriteLine("Snake: Go Back");
+                            if (player_2_Position < 0)
+                            {
+                                player_2_Position = 0;
+                            }
+                            break;
+                    }
+                    chance2 += 1;
+                    Console.WriteLine("You're New Position - " + player_2_Position);
+                    turn -= 1;
+                }
             }
-            Console.WriteLine("Count of Dice Played: "+chance);
-            Console.WriteLine("*****You have won the Game*****");
+            Console.WriteLine("Count of Dice Played by Player1: "+chance1);
+            Console.WriteLine("Count of Dice Played by Player2: " + chance2);
+            if (player_1_Position == 100)
+            {
+                Console.WriteLine("*****Player1 have won the Game*****");
+            }
+            else
+            {
+                Console.WriteLine("*****Player2 have won the Game*****");
+            }
         }
     }
 }
