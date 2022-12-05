85e1955283bc23c1df7d24da92e18371cb837b67
rajpal229 <rajpalrawat229@gmail.com>
2022-12-05 15:56:56 +0530

    Added: UC1_PlayerPosition

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Snake_And_Ladder_Problem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Problem");
+            Gameplay playerPosition = new Gameplay();
+            playerPosition.Position();
+        }
+    }
+}
